d2da2f4584d8d3da6b5681d030cd5317d1278586
ASCDarshan <ascdarshan@gmail.com>
2025-05-19 15:49:13 +0530

    add tidio chat

--- src/components/TidioChat/TidioChat.js
+++ src/components/TidioChat/TidioChat.js
@@ -1,22 +1,22 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// const TidioChat = () => {
-//   useEffect(() => {
-//     // Create the script element
-//     const script = document.createElement("script");
-//     script.src = "//code.tidio.co/l4otkjmkeqnqu0g5wnjg17ibrfhfnxqw.js";
-//     script.async = true;
+const TidioChat = () => {
+  useEffect(() => {
+    // Create the script element
+    const script = document.createElement("script");
+    script.src = "//code.tidio.co/wueeymfuog4qp5r61hon5sv4aoaioxdr.js";
+    script.async = true;
 
-//     // Append the script to the body
-//     document.body.appendChild(script);
+    // Append the script to the body
+    document.body.appendChild(script);
 
-//     // Cleanup function to remove the script when the component unmounts
-//     return () => {
-//       document.body.removeChild(script);
-//     };
-//   }, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
 
-//   return null; // This component doesn't render anything visible
-// };
+  return null; // This component doesn't render anything visible
+};
 
-// export default TidioChat;
+export default TidioChat;
